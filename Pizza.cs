a7b5270d38aa87e45917a0f250a36094cf0a1331
Omar-Atriano <omar.atrianova@udlap.mx>
2023-03-02 11:38:35 -0600

    Limpieza pizza

--- Pizza.cs
+++ Pizza.cs
@@ -9,29 +9,22 @@ namespace Graphic_Engine
     public class Pizza
     {
         public int radious;
-        //List<PointF3D> points = new List<PointF3D>();
         public Pizza(int rad, ref Mesh mesh)
         {
             this.radious = rad;
             
-            //List<int> index = new List<int>();
             int rebanadas = 20;
-            //double r = 1;
+            int height = 0;
             double anguloInicial = 0;
             double anguloFinal = (360 / rebanadas) * (Math.PI / 180);
 
-            //points.Add(new PointF3D(0, 0, 0));
 
             for(int i = 0; i< rebanadas; i++)
             {
                 Triangle triangulo = new Triangle();
-                //points.Add(new PointF3D(0, 0, 0));
-                //points.Add(new PointF3D((float)(r * Math.Cos(anguloInicial)), (float)(r * Math.Sin(anguloInicial)), 0));
-                triangulo.Add(new PointF3D(0, 0, 0));
-                triangulo.Add(new PointF3D((float)(radious * Math.Cos(anguloInicial)), (float)(radious * Math.Sin(anguloInicial)), 0));
-                triangulo.Add(new PointF3D((float)(radious * Math.Cos(anguloFinal)), (float)(radious * Math.Sin(anguloFinal)), 0));
-                //points.Add(new PointF3D((float)(radious * Math.Cos(anguloFinal)), (float)(radious * Math.Sin(anguloFinal)), 0));
-                //points.Add(new PointF3D((float)(radious * Math.Cos(anguloFinal)), (float)(radious * Math.Sin(anguloFinal)), 0));
+                triangulo.Add(new PointF3D(0, 0, height));
+                triangulo.Add(new PointF3D((float)(radious * Math.Cos(anguloInicial)), (float)(radious * Math.Sin(anguloInicial)), height));
+                triangulo.Add(new PointF3D((float)(radious * Math.Cos(anguloFinal)), (float)(radious * Math.Sin(anguloFinal)), height));
 
                 mesh.Figures.Add(triangulo);
 
