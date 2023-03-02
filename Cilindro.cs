29b05087361ab491d7b580ffe391fcfd8792ef49
Omar-Atriano <omar.atrianova@udlap.mx>
2023-03-02 11:25:28 -0600

    tururur

---
+++ Cilindro.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphic_Engine
+{
+    public class Cilindro
+    {
+        private int radious;
+        private int height;
+        public PointF3D top;
+        public Point top2D;
+        public Cilindro(int radious, int height, ref Mesh mesh)
+        {
+            this.radious = radious;
+            this.height = height;
+
+            //top = new PointF3D(0, 0, height);
+
+            //Pizza de abajo 
+            Pizza pizzaB = new Pizza(radious, ref mesh);
+            
+            
+            //Pizza de arriba
+            Pizza pizzaT = new Pizza(radious, ref mesh);
+
+        }
+    }
+}
