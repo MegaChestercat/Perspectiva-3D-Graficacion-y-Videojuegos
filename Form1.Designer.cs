561e7f9e29a24c534c708fe4789469d27e5b041a
Omar-Atriano <omar.atrianova@udlap.mx>
2023-03-04 16:22:46 -0600

    Media esfera

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -38,14 +38,14 @@
             this.label1 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.panel4 = new System.Windows.Forms.Panel();
             this.panel5 = new System.Windows.Forms.Panel();
-            this.buttonCube = new System.Windows.Forms.Button();
-            this.buttonCone = new System.Windows.Forms.Button();
-            this.buttonCylinder = new System.Windows.Forms.Button();
-            this.buttonSphere = new System.Windows.Forms.Button();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
             this.buttonHalfSphere = new System.Windows.Forms.Button();
+            this.buttonSphere = new System.Windows.Forms.Button();
+            this.buttonCylinder = new System.Windows.Forms.Button();
+            this.buttonCone = new System.Windows.Forms.Button();
+            this.buttonCube = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.PCT_CANVAS)).BeginInit();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
@@ -57,7 +57,7 @@
             this.PCT_CANVAS.BackColor = System.Drawing.Color.Black;
             this.PCT_CANVAS.Location = new System.Drawing.Point(72, 46);
             this.PCT_CANVAS.Name = "PCT_CANVAS";
-            this.PCT_CANVAS.Size = new System.Drawing.Size(1021, 531);
+            this.PCT_CANVAS.Size = new System.Drawing.Size(1273, 722);
             this.PCT_CANVAS.TabIndex = 0;
             this.PCT_CANVAS.TabStop = false;
             // 
@@ -123,9 +123,9 @@
             this.panel1.Controls.Add(this.rotBTN4);
             this.panel1.Controls.Add(this.rotBTN3);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
-            this.panel1.Location = new System.Drawing.Point(1093, 0);
+            this.panel1.Location = new System.Drawing.Point(1345, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(200, 673);
+            this.panel1.Size = new System.Drawing.Size(200, 868);
             this.panel1.TabIndex = 6;
             // 
             // panel2
@@ -137,8 +137,25 @@
             this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel2.Location = new System.Drawing.Point(0, 0);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1093, 673);
+            this.panel2.Size = new System.Drawing.Size(1345, 868);
             this.panel2.TabIndex = 7;
+            // 
+            // panel5
+            // 
+            this.panel5.Cursor = System.Windows.Forms.Cursors.SizeWE;
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel5.Location = new System.Drawing.Point(0, 46);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(75, 722);
+            this.panel5.TabIndex = 3;
+            // 
+            // panel4
+            // 
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel4.Location = new System.Drawing.Point(0, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(1345, 46);
+            this.panel4.TabIndex = 2;
             // 
             // panel3
             // 
@@ -148,63 +165,10 @@
             this.panel3.Controls.Add(this.buttonCone);
             this.panel3.Controls.Add(this.buttonCube);
             this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel3.Location = new System.Drawing.Point(0, 573);
+            this.panel3.Location = new System.Drawing.Point(0, 768);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(1093, 100);
+            this.panel3.Size = new System.Drawing.Size(1345, 100);
             this.panel3.TabIndex = 1;
-            // 
-            // panel4
-            // 
-            this.panel4.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel4.Location = new System.Drawing.Point(0, 0);
-            this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(1093, 46);
-            this.panel4.TabIndex = 2;
-            // 
-            // panel5
-            // 
-            this.panel5.Cursor = System.Windows.Forms.Cursors.SizeWE;
-            this.panel5.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panel5.Location = new System.Drawing.Point(0, 46);
-            this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(75, 527);
-            this.panel5.TabIndex = 3;
-            // 
-            // buttonCube
-            // 
-            this.buttonCube.Location = new System.Drawing.Point(121, 31);
-            this.buttonCube.Name = "buttonCube";
-            this.buttonCube.Size = new System.Drawing.Size(75, 23);
-            this.buttonCube.TabIndex = 0;
-            this.buttonCube.Text = "Cube";
-            this.buttonCube.UseVisualStyleBackColor = true;
-            // 
-            // buttonCone
-            // 
-            this.buttonCone.Location = new System.Drawing.Point(313, 30);
-            this.buttonCone.Name = "buttonCone";
-            this.buttonCone.Size = new System.Drawing.Size(75, 23);
-            this.buttonCone.TabIndex = 1;
-            this.buttonCone.Text = "Cone";
-            this.buttonCone.UseVisualStyleBackColor = true;
-            // 
-            // buttonCylinder
-            // 
-            this.buttonCylinder.Location = new System.Drawing.Point(498, 30);
-            this.buttonCylinder.Name = "buttonCylinder";
-            this.buttonCylinder.Size = new System.Drawing.Size(75, 23);
-            this.buttonCylinder.TabIndex = 2;
-            this.buttonCylinder.Text = "Cylinder";
-            this.buttonCylinder.UseVisualStyleBackColor = true;
-            // 
-            // buttonSphere
-            // 
-            this.buttonSphere.Location = new System.Drawing.Point(696, 30);
-            this.buttonSphere.Name = "buttonSphere";
-            this.buttonSphere.Size = new System.Drawing.Size(75, 23);
-            this.buttonSphere.TabIndex = 3;
-            this.buttonSphere.Text = "Sphere";
-            this.buttonSphere.UseVisualStyleBackColor = true;
             // 
             // buttonHalfSphere
             // 
@@ -215,12 +179,48 @@
             this.buttonHalfSphere.Text = "Half Sphere";
             this.buttonHalfSphere.UseVisualStyleBackColor = true;
             // 
+            // buttonSphere
+            // 
+            this.buttonSphere.Location = new System.Drawing.Point(696, 30);
+            this.buttonSphere.Name = "buttonSphere";
+            this.buttonSphere.Size = new System.Drawing.Size(75, 23);
+            this.buttonSphere.TabIndex = 3;
+            this.buttonSphere.Text = "Sphere";
+            this.buttonSphere.UseVisualStyleBackColor = true;
+            // 
+            // buttonCylinder
+            // 
+            this.buttonCylinder.Location = new System.Drawing.Point(498, 30);
+            this.buttonCylinder.Name = "buttonCylinder";
+            this.buttonCylinder.Size = new System.Drawing.Size(75, 23);
+            this.buttonCylinder.TabIndex = 2;
+            this.buttonCylinder.Text = "Cylinder";
+            this.buttonCylinder.UseVisualStyleBackColor = true;
+            // 
+            // buttonCone
+            // 
+            this.buttonCone.Location = new System.Drawing.Point(313, 30);
+            this.buttonCone.Name = "buttonCone";
+            this.buttonCone.Size = new System.Drawing.Size(75, 23);
+            this.buttonCone.TabIndex = 1;
+            this.buttonCone.Text = "Cone";
+            this.buttonCone.UseVisualStyleBackColor = true;
+            // 
+            // buttonCube
+            // 
+            this.buttonCube.Location = new System.Drawing.Point(121, 31);
+            this.buttonCube.Name = "buttonCube";
+            this.buttonCube.Size = new System.Drawing.Size(75, 23);
+            this.buttonCube.TabIndex = 0;
+            this.buttonCube.Text = "Cube";
+            this.buttonCube.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.ClientSize = new System.Drawing.Size(1293, 673);
+            this.ClientSize = new System.Drawing.Size(1545, 868);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
---
+++ HalfSphere.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphic_Engine
+{
+    //internal class HalfSphere
+
+    public class HalfSphere
+    {
+
+        private int numSegments;
+        private float radius;
+
+        public HalfSphere(float radius, int numSegments, ref Mesh mesh)
+        {
+            this.radius = radius;
+            this.numSegments = numSegments;
+            float angle = 360 / numSegments;
+            float deltaRadius = radius / numSegments;
+            float x = (float)Math.Cos((Math.PI / 180) * (angle / 2)) * radius;
+            float y = (float)Math.Sin((Math.PI / 180) * (angle / 2)) * radius;
+
+
+            // Create the triangles that form the surface of the sphere
+            for (int i = 0; i < numSegments + 1; i++)
+            {
+
+                float lat0 = (float)Math.PI * (-0.5f + (float)(i - 1) / numSegments);
+                float z0 = (float)Math.Sin(lat0) * radius;
+                float zr0 = (float)Math.Cos(lat0) * radius;
+
+                float lat1 = (float)Math.PI * (-0.5f + (float)i / numSegments);
+                float z1 = (float)Math.Sin(lat1) * radius;
+                float zr1 = (float)Math.Cos(lat1) * radius;
+
+                for (int j = 0; j < numSegments/2; j++)
+                {
+                    float lng0 = (float)(2 * Math.PI * (float)(j - 1) / numSegments);
+                    float x0 = (float)Math.Cos(lng0) * zr0;
+                    float y0 = (float)Math.Sin(lng0) * zr0;
+
+                    float lng1 = (float)(2 * Math.PI * (float)j / numSegments);
+                    float x1 = (float)Math.Cos(lng1) * zr0;
+                    float y1 = (float)Math.Sin(lng1) * zr0;
+
+                    float x2 = (float)Math.Cos(lng0) * zr1;
+                    float y2 = (float)Math.Sin(lng0) * zr1;
+
+                    float x3 = (float)Math.Cos(lng1) * zr1;
+                    float y3 = (float)Math.Sin(lng1) * zr1;
+
+                    PointF3D p0 = new PointF3D(x0, y0, z0);
+                    PointF3D p1 = new PointF3D(x1, y1, z0);
+                    PointF3D p2 = new PointF3D(x2, y2, z1);
+                    PointF3D p3 = new PointF3D(x3, y3, z1);
+                    if (i == 0) { }
+                    else if (i == 1)
+                    {
+                        Triangle t = new Triangle();
+                        t.Add(p3);
+                        t.Add(p2);
+                        t.Add(p3);
+
+                        mesh.Figures.Add(t);
+                    }
+                    else
+                    {
+                        Triangle t = new Triangle();
+                        t.Add(p0);
+                        t.Add(p1);
+                        t.Add(p2);
+
+                        mesh.Figures.Add(t);
+                    }
+
+                }
+            }
+
+
+
+
+        }
+
+
+    }
+
+}
